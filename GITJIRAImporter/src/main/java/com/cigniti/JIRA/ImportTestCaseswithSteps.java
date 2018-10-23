34258f5d003b855d64d376d74e0399834b184f15
RAC\duraka <Akarsh.Durgekar@rentacenter.com>
2018-10-23 11:48:49 -0500

    Version 2.1 update

--- GITJIRAImporter/src/main/java/com/cigniti/JIRA/ImportTestCaseswithSteps.java
+++ GITJIRAImporter/src/main/java/com/cigniti/JIRA/ImportTestCaseswithSteps.java
@@ -65,7 +65,7 @@ public class ImportTestCaseswithSteps{
 	private SwingWorker<Void, String> bgWorker;
 	public static String DisplayMessage = "Success:All test cases are uploaded successfully";
 	public String appName = "Jira Test Case Importer";
-	public String versionNumber = "";
+	public String versionNumber = "2.1";
 	public String pageName = "Login";
 	public String TestCaseIdColumn = "TestCaseId";
 	public String strAuthenticationMessage;
